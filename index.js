42fd9c1ae24bcb9bf0e38436d6be18124a613779
mitchell williams <mscottwilliams1994@gmail.com>
2019-06-13 17:35:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,15 +10,30 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ // write your code herev
+var price = Math.ceil(Math.random()*100)
+cart.push({[item]: price})
+console.log(item+` has been added to your cart.`)
+return cart
+  
 }
-
+   
 function viewCart() {
   // write your code here
+if (cart.length ===0){
+  console.log('Your shopping cart is empty.')
+}
+ var newcart =[];
+ for (var i = 0;i<cart.length; i++){
+   var key = object.key(car[i])[0]
+   newcart.push(key+" at $"+cart[i][key])
+ }
+ 
 }
 
 function total() {
   // write your code here
+
 }
 
 function removeFromCart(item) {
@@ -28,3 +43,4 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
 }
+var cart = [];
